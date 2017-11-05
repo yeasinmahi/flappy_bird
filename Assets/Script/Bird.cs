4f089740079f0ff08955fc89ca998bfb281c9866
yeasinmahi <yeasinmahi72@yahoo.com>
2017-11-05 20:42:41 +0600

    init

---
+++ Assets/Script/Bird.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+	public float upForce = 200f;
+
+	private bool isDead = false;
+	private Rigidbody2D rb2d;
+	private Animator anim;
+
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	
+	}
+
+	void Update (){
+		if(!isDead){
+			if(Input.GetMouseButtonDown(0)){
+				rb2d.velocity = Vector2.zero;
+				rb2d.AddForce(new Vector2 (0,upForce));
+				anim.SetTrigger("Flap");
+			}
+		}
+	}
+	void OnCollisionEnter2D(){
+		isDead = true;
+		anim.SetTrigger("Die");
+	}
+}
